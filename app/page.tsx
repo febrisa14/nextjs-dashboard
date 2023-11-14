ba5fec031bf86da40c308444bac147cd2a4c1724
Febrisa Permana <78676283+febrisa14@users.noreply.github.com>
2023-11-14 11:23:59 +0800

    update invoice page
    search
    query param
    use debounce

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
-
 import Link from 'next/link';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">

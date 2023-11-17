4556c4debdf0fc89292be43bad3f385c7caaa095
Febrisa Permana <78676283+febrisa14@users.noreply.github.com>
2023-11-17 11:43:54 +0800

    update customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,31 @@
-export default function Customer() {
+import Search from "@/app/ui/search";
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchCustomersPages, fetchFilteredCustomers } from "@/app/lib/data";
+import Pagination from "@/app/ui/invoices/pagination";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function Customer({ searchParams }: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    }
+}) {
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page || 1);
+    const totalPages = await fetchCustomersPages(query);
     return (
-        <div>Customer Page</div>
+        <div className="w-full">
+            <h1 className={`mb-8 text-xl md:text-2xl`}>
+                Customers
+            </h1>
+            <Search placeholder="Search customers..." />
+            <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+                <CustomersTable query={query} currentPage={currentPage} />
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </div>
     )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -193,9 +193,10 @@ export async function fetchCustomers() {
   }
 }
 
-export async function fetchFilteredCustomers(query: string) {
+export async function fetchFilteredCustomers(query: string, currentPage: number) {
   noStore();
   try {
+    const offset = (currentPage - 1) * ITEMS_PER_PAGE;
     const data = await sql<CustomersTable>`
 		SELECT
 		  customers.id,
@@ -212,6 +213,7 @@ export async function fetchFilteredCustomers(query: string) {
         customers.email ILIKE ${`%${query}%`}
 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
 		ORDER BY customers.name ASC
+    LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
 	  `;
 
     const customers = data.rows.map((customer) => ({
@@ -224,6 +226,25 @@ export async function fetchFilteredCustomers(query: string) {
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
+  }
+}
+
+export async function fetchCustomersPages(query: string) {
+  noStore();
+  try {
+    const count = await sql`
+		SELECT COUNT(*)
+		FROM customers
+		WHERE
+		name ILIKE ${`%${query}%`} OR
+    email ILIKE ${`%${query}%`}
+	  `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of customers.');
   }
 }
 
